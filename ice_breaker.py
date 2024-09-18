813c2031422459c6d5a163e0536da7298545118f
CollegeDream <hminh1320@gmail.com>
2024-09-18 10:36:08 -0700

    added files

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,8 +1,8 @@
 from langchain.prompts.prompt import PromptTemplate
 from langchain_openai import ChatOpenAI
 from langchain_core.output_parsers import StrOutputParser
-from langchain_ollama import ChatOllama
-from langchain_community import 
+# from langchain_ollama import ChatOllama
+# from langchain_community import 
 
 information = """
     Elon Reeve Musk FRS (/ˈiːlɒn/; born June 28, 1971) is a businessman and investor, known for his key roles in the space company SpaceX and the automotive company Tesla, Inc. Other involvements include ownership of X Corp., the company that operates the social media platform X (formerly known as Twitter), and his role in the founding of The Boring Company, xAI, Neuralink, and OpenAI. He is one of the wealthiest individuals in the world; as of August 2024 Forbes estimates his net worth to be US$247 billion.
@@ -19,7 +19,7 @@ if __name__ == "__main__":
 
     summary_prompt_template = PromptTemplate(input_variables=["information"], template=summary_template )
     
-    # llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo", openai_api_key = "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" )
+    # llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo", )
     llm = ChatOllama(model="llama3.1")
     
     chain = summary_prompt_template | llm
